b5b707f4304441de4de2f8e3f51774632ecf0098
Sara Gilani <saragilani@Saras-MacBook-Pro.local>
2022-12-02 09:38:21 +0000

    changed q 3 in extension, removed inner join with enclosure table

--- safari_queries.sql
+++ safari_queries.sql
@@ -13,6 +13,6 @@ INNER JOIN enclosures ON enclosures.enclosure_id = assignments.enclosure_id WHER
 SELECT enclosures.name FROM animals INNER JOIN enclosures ON enclosures.enclosure_id = animals.enclosure_id ORDER BY animals.age DESC LIMIT 1;
 
 -- 3.
-SELECT Count(a.type) FROM animals AS a INNER JOIN enclosures AS e ON e.enclosure_id = a.enclosure_id 
-INNER JOIN assignments ON e.enclosure_id = assignments.enclosure_id 
+SELECT Count(a.type) FROM animals AS a
+INNER JOIN assignments ON a.enclosure_id = assignments.enclosure_id 
 INNER JOIN staff ON staff.staff_id = assignments.employee_id WHERE staff.name = 'Salma';
